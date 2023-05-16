7be09f2f29ab1538077428f9985fe73ebde90ce1
Siddharth Kannan <mail@siddharthkannan.in>
2023-05-16 18:38:07 +0900

    Add comments about why each conversion is required

--- html-to-epub.sh
+++ html-to-epub.sh
@@ -31,6 +31,13 @@ node $script "$input" "$dir/$output_html" "$dir/$output_metadata"
 # Pandock might be using Docker, which uses --volume to mount `pwd` as /data. So, we should change
 # directory to the appropriate place before executing the conversino scripts.
 cd $dir && \
+	# HTML => XHTML
+	# HTML from websites might have things like <img src="..."> (unclosed img tags).
+	# These can not be parsed by Pandoc and Pandoc will stop parsing at the first mismatched close tag.
+	# To avoid this case, we will convert from HTML => XHTML. XHTML is valid XML and has stricter
+	# requirements, which match the input requirements for Pandoc.
 	pandock --output $output_xhtml $output_html && \
+	# HTML => EPUB
+	# The Epub metadata (title, author) is provided as an XML file.
 	pandock --output $output_epub --epub-metadata "$output_metadata" "$output_xhtml" && \
 	echo "Ouptut file written to $(pwd)/$output_epub"
