8dadb5514360a0a4b49b1b60c6a56d256f0c0484
Siddharth Kannan <mail@siddharthkannan.in>
2025-10-17 22:20:36 +0900

    feat: Output text file when requested

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const helpText = `Example: node main.js input-file output-file output-metdata-file [debug]
+const helpText = `Example: node main.js input-file output-file output-metdata-file [debug] [text-output-file]
 
-  input-file: HTML file containing the input markup
-  output-file: HTML file containing the output markup (processed using readability and DOMpurify)
-  output-metadata-file: YAML formatted file with metadata about the content (title, author, language)
-  debug: Optionally, set this to "debug" to print debug logs
+  input-file: HTML file containing the input markup. Required.
+  output-file: HTML file containing the output markup (processed using readability and DOMpurify). Required.
+  output-metadata-file: YAML formatted file with metadata about the content (title, author, language). Required.
+  debug: Set this to "yes" to print debug logs. Optional. Default: no.
+  output-text-file: Set this to write out the text content of the article. Optional.
 `
 
 if (process.argv.length < 5) {
@@ -25,7 +26,8 @@ ${helpText}
 	process.exit(1);
 }
 
-const debug = process.argv.length >= 6 && process.argv[5] === 'debug';
+const debug = process.argv.length >= 6 && process.argv[5] === 'yes';
+const outputTextFile = process.argv.length >= 7 ? process.argv[6] : '';
 
 const { Readability } = require('@mozilla/readability');
 const { JSDOM } = require('jsdom');
@@ -43,6 +45,15 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
 	});
 	let reader = new Readability(doc.window.document);
 	let article = reader.parse();
+	if (outputTextFile !== '') {
+		fs.writeFile(outputTextFile, article.textContent, 'utf8', (err) => {
+			if (err) {
+				throw err;
+			} else {
+				console.log('written to output text file');
+			}
+		});
+	}
 	const metadata = `<dc:title>${article.title}</dc:title>
 <dc:publisher>${article.siteName}</dc:publisher>
 <dc:creator>${article.byline}</dc:creator>
